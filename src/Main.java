373ec9d2898195321fac6be6c770deb45c0adafe
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-03-27 22:28:53 +0200

    [Solution #1] Write a Java program to print 'Hello' on screen and then print your name on a separate line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+/* 1. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+
+        System.out.println("Hello\nALexandra Abramov");
     }
 }
