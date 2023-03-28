9e1ea4c48417b0e154deeba703b8803c2b3165c4
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-03-28 20:59:35 +0200

    [Solution #4] Write a Java program to print the result of the following operations

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-/* 3. Write a Java program to divide two numbers and print on the screen.
-Test Data :
-50/3
+/* 4. Write a Java program to print the result of the following operations.
+Test Data:
+a. -5 + 8 * 6
+b. (55+9) % 9
+c. 20 + -3*5 / 8
+d. 5 + 15 / 3 * 2 - 8 % 3
 Expected Output :
-16*/
+43
+1
+19
+13*/
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(50/3);
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
 
     }
 }
