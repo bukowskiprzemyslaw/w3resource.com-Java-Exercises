edc850115b483d32b540212ebd6f15bd1cf558ba
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-03-30 20:09:25 +0200

    [Solution #8] Write a Java program to display the following pattern

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 /*
-7. Write a Java program that takes a number as input and prints its multiplication table upto 10.
-Test Data:
-Input a number: 8
-Expected Output :
-8 x 1 = 8
-8 x 2 = 16
-8 x 3 = 24
-...
-8 x 10 = 80
+8. Write a Java program to display the following pattern. Go to the editor
+Sample Pattern :
+
+   J    a   v     v  a
+   J   a a   v   v  a a
+J  J  aaaaa   V V  aaaaa
+ JJ  a     a   V  a     a
 */
 
 import java.util.Scanner;
@@ -15,13 +13,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        System.out.println("   J    a   v     v  a");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
 
-        System.out.println("Input number you want to multiply");
-
-        int number = input.nextInt();
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(number + " * " + i + " = " + (number * i));
-        }
     }
 }
