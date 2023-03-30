8c36c172238aba42afeb35743c7ccfcd459ad1c8
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-03-30 20:04:15 +0200

    [Solution #7] Write a Java program that takes a number as input and prints its multiplication table upto 10

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 /*
-6. Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
-Input first number: 125
-Input second number: 24
+7. Write a Java program that takes a number as input and prints its multiplication table upto 10.
+Test Data:
+Input a number: 8
 Expected Output :
-125 + 24 = 149
-125 - 24 = 101
-125 x 24 = 3000
-125 / 24 = 5
-125 mod 24 = 5
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24
+...
+8 x 10 = 80
 */
 
 import java.util.Scanner;
@@ -17,26 +17,11 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Input first number");
-        int number1 = input.nextInt();
+        System.out.println("Input number you want to multiply");
 
-        System.out.println("Input second number");
-        int number2 = input.nextInt();
-
-        System.out.println("First number + second number");
-        System.out.println("= " + (number1 + number2));
-
-        System.out.println("First number - second number");
-        System.out.println("= " + (number1 - number2));
-
-        System.out.println("First number * second number");
-        System.out.println("= " + (number1 * number2));
-
-        System.out.println("First number / second number");
-        System.out.println("= " + (number1 / number2));
-
-        System.out.println("First number mod second number");
-        System.out.println("= " + (number1 % number2));
-
+        int number = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
     }
 }
