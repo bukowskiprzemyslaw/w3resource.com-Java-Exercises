8d95bf943446bceaa0e7366637fcc1a1ae8a978d
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-03-30 20:14:54 +0200

    [Solution #9] Write a Java program to compute the specified expressions and print the output

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
 /*
-8. Write a Java program to display the following pattern. Go to the editor
-Sample Pattern :
-
-   J    a   v     v  a
-   J   a a   v   v  a a
-J  J  aaaaa   V V  aaaaa
- JJ  a     a   V  a     a
+9. Write a Java program to compute the specified expressions and print the output.
+Test Data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+Expected Output
+2.138888888888889
 */
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("   J    a   v     v  a");
-        System.out.println("   J   a a   v   v  a a");
-        System.out.println("J  J  aaaaa   V V  aaaaa");
-        System.out.println(" JJ  a     a   V  a     a");
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
 
     }
 }
