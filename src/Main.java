0aff0822ba6dc8964b3883e3ed298e9e458d78c2
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-04-01 21:27:37 +0200

    [Solution #12] Write a Java program that takes three numbers as input to calculate and print the average of the numbers

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,32 @@
 /*
-11. Write a Java program to print the area and perimeter of a circle. Go to the editor
-Test Data:
-Radius = 7.5
-Expected Output
-Perimeter is = 47.12388980384689
-Area is = 176.71458676442586
+12. Write a Java program that takes three numbers as input to calculate and print the average of the numbers.
 */
 
+import java.util.Scanner;
+
 public class Main {
-
-    private static final double radius = 7.5;
-
     public static void main(String[] args) {
 
-            double perimeter = 2 * Math.PI * radius;
-            double area = Math.PI * radius * radius;
+        Scanner in = new Scanner(System.in);
 
-            System.out.println("Perimeter is = " + perimeter);
-            System.out.println("Area is = " + area);
+        System.out.print("Input first number: ");
+        int number1 = in.nextInt();
+
+        System.out.print("Input second number: ");
+        int number2 = in.nextInt();
+
+        System.out.print("Input third number: ");
+        int number3 = in.nextInt();
+
+        System.out.print("Input fourth number: ");
+        int number4 = in.nextInt();
+
+        System.out.print("Enter fifth number: ");
+        int number5 = in.nextInt();
+
+
+        System.out.println("Average of five numbers is: " +
+                (number1 + number2 + number3 + number4 + number5) / 5);
     };
 
 }
