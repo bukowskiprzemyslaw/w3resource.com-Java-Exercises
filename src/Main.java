65ca851b10d16a3d4ec21663da22d5394316b5ce
Bukowski <bukowski.przemyslaw.1988@gmail.com>
2023-06-22 22:39:36 +0200

    [Solution #18] Write a Java program to multiply two binary numbers

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,64 @@
 /*
-17. Write a Java program to add two binary numbers.
-Input Data:
-Input first binary number: 10
-Input second binary number: 11
-Expected Output
+18. Write a Java program to multiply two binary numbers.
 
-Sum of two binary numbers: 101
+In digital electronics and mathematics, a binary number is a number expressed in the base-2 numeral system or binary numeral system. This system uses only two symbols: typically 1 (one) and 0 (zero).
+
+Test Data:
+Input first binary number: 110
+Input second binary number: 101
 */
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-
-        long binary1, binary2;
+    public static void main(String[] args)
+    {
+        long binary1, binary2, multiply = 0;
+        int digit, factor = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first binary number: ");
+        binary1 = in.nextLong();
+        System.out.print("Input the second binary number: ");
+        binary2 = in.nextLong();
+        while (binary2 != 0)
+        {
+            digit = (int)(binary2 % 10);
+            if (digit == 1)
+            {
+                binary1 = binary1 * factor;
+                multiply = binaryproduct((int) binary1, (int) multiply);
+            }
+            else
+            {
+                binary1 = binary1 * factor;
+            }
+            binary2 = binary2 / 10;
+            factor = 10;
+        }
+        System.out.print("Product of two binary numbers: " + multiply+"\n");
+    }
+    static int binaryproduct(int binary1, int binary2)
+    {
         int i = 0, remainder = 0;
         int[] sum = new int[20];
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Input first binary number: ");
-        binary1 = in.nextLong();
-        System.out.print("Input second binary number: ");
-        binary2 = in.nextLong();
+        int binary_prod_result = 0;
 
         while (binary1 != 0 || binary2 != 0)
         {
-            sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
-            remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);
+            sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
+            remainder = (binary1 % 10 + binary2 % 10 + remainder) / 2;
             binary1 = binary1 / 10;
             binary2 = binary2 / 10;
         }
-        if (remainder != 0) {
+        if (remainder != 0)
+        {
             sum[i++] = remainder;
         }
         --i;
-        System.out.print("Sum of two binary numbers: ");
-        while (i >= 0) {
-            System.out.print(sum[i--]);
+        while (i >= 0)
+        {
+            binary_prod_result = binary_prod_result * 10 + sum[i--];
         }
-        System.out.print("\n");
-
+        return binary_prod_result;
     }
 }
